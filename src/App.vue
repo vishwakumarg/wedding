7fea235286b52388995f12219e0b015470080070
vishwakumar <48910267+vishwakumarg@users.noreply.github.com>
2023-05-15 11:54:32 +0530

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -184,7 +184,7 @@
             <div class="gla_icon_boxes row text-center">
               <div class="gla_map">
                 <iframe
-                  src= "https://goo.gl/maps/7tDRJtrbNJ1ERLE8A?coh=178573&entry=tt"
+                  src= "https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d1211.3722539539383!2d73.94051311609894!3d16.86603906124862!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMTbCsDUxJzU4LjUiTiA3M8KwNTYnMjUuOSJF!5e0!3m2!1sen!2sin!4v1684131823621!5m2!1sen!2sin"
                   width="100%"
                   height="500"
                   style="padding: 50px 0;"
